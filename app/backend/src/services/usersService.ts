a23ac735d8e16b379818a54522b571cb2bff21ed
JohnnyNWT <jooaaooskt@gmail.com>
2023-05-27 14:17:05 -0300

    FEAT: valida login

--- app/backend/src/services/usersService.ts
+++ app/backend/src/services/usersService.ts
@@ -11,11 +11,17 @@ class usersService {
   }
 
   async findUser(email: string, password: string): Promise<LoginType> {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordLength = password.length < 6;
     const userEmail = await this._usersModel.findOne({ where: { email } });
+
+    if (!userEmail || !emailRegex.test(email)) {
+      return { message: 'Invalid email or password', token: '' };
+    }
 
     const verifyPassword = await bcrypt.compare(password, userEmail?.dataValues.password);
 
-    if (!verifyPassword) {
+    if (passwordLength || !verifyPassword) {
       return { message: 'Invalid email or password', token: '' };
     }
 
